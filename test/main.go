627b19323e2a5f5f0d122b6fc5091ec4dcd185d6
Rock Chin <1010553892@qq.com>
2022-12-18 01:15:42 +0800

    test

--- test/main.go
+++ test/main.go
@@ -1,12 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func InputString(prompt string) string {
+	print(prompt)
+	str := ""
+
+	fmt.Scanf("%s", &str)
+
+	if len(str) < 1 || str == "\n" || str == "" || str == " " || str == "\r" || str == "\t" || str == "\b" {
+		fmt.Scanf("%s", &str)
+	}
+
+	return str
+}
 
 func main() {
 
-	api_key := ""
-	print("请输入api_key: ")
-	fmt.Scanf("%s", &api_key)
+	re, err := regexp.Compile("sk[-][a-zA-Z0-9]{48}")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		input := InputString("请输入OpenAI账号的api_key: ")
+		input = strings.Trim(input, " \n\r\t\b")
+		fmt.Println("input:", input)
+		// println("bytes:", []byte(input)[0])
+		if re.MatchString(input) {
+			println("api_key格式正确")
+			break
+		} else if input != "" && input != "\n" {
+			println("api_key格式错误")
+		}
+	}
 
 	qqn := 0
 	print("请输入QQ号: ")
